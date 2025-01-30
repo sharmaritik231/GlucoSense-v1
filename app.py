5ee8ee6bb11bf4fbd4c8da99f6df78ae919ae901
sharmaritik231 <s23111@students.iitmandi.ac.in>
2025-01-30 13:12:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,12 @@ def main():
     name = st.text_input("Name")
     age = st.number_input("Age", min_value=0)
     gender = st.selectbox("Gender", options=["Male", "Female", "Other"])
+    gender = 0 if gender == "Male" else 1
     heart_rate = st.number_input("Heart Rate", min_value=0)
     max_bp = st.number_input("Max BP", min_value=0)
     min_bp = st.number_input("Min BP", min_value=0)
-    spo2 = st.number_input("SPO2", min_value=0.0, max_value=100.0, format="%.1f")
+    spo2 = st.number_input("SPO2", min_value=0, max_value=100)
+    body_vitals = {'Age': [age], 'Gender': [gender], 'Heart_Beat': [heart_rate], 'SPO2': [spo2], 'max_BP': [max_bp], 'min_BP': [min_bp]}
 
     # File upload
     uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
@@ -23,13 +25,7 @@ def main():
             data = pd.read_csv(uploaded_file, skiprows=3).iloc[:, 1:]
 
             # Generate data for the test
-            data['Age'] = age
-            data['Gender'] = gender
-            data['Heart_Beat'] = heart_rate
-            data['SPO2'] = spo2
-            data['max_BP'] = max_bp
-            data['min_BP'] = min_bp
-            test_data = commons.generate_data(data)
+            test_data = commons.generate_data(data, body_vitals)
 
             # Perform tests
             diabetes_result = commons.perform_diabetes_test(test_data)
